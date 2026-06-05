4bd52867bf296ae932aa987976f30652c7892d69
Ankur Kesharwani <ankur.kesharwani001@gmail.com>
2026-06-05 22:29:29 +0530

    Adding lualine

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -121,6 +121,29 @@ return {
     end,
   },
 
+  -- Statusline
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup({
+        options = {
+          theme = "auto",
+          component_separators = { left = "", right = "" },
+          section_separators = { left = "", right = "" },
+        },
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = { "branch", "diff", "diagnostics" },
+          lualine_c = { { "filename", path = 1 } },
+          lualine_x = { "encoding", "fileformat", "filetype" },
+          lualine_y = { "progress" },
+          lualine_z = { "location" },
+        },
+      })
+    end,
+  },
+
   -- Buffer close without closing the window
   { "famiu/bufdelete.nvim" },
 
